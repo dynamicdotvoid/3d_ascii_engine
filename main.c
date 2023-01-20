4640dbd5a463e4072323f2ff9cbb79f91737367a
Lucas Merritt <lucas.j.merritt@gmail.com>
2023-01-20 15:56:43 -0700

    primitive screen functionality

--- main.c
+++ main.c
@@ -1,46 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <errno.h>
 
-int msleep(long period);
+// 500 x 500 "screen" size
+#define MAX_WIDTH 500
+#define MAX_HEIGHT 500
 
 int main() {
     // Clear the screen
     printf("\x1b[2J");
+    // Move cursor to home position
+    printf("\x1b[H");
 
-    /* for(int i;;i++) {
-        // Move cursor to home position
-        //printf("\x1b[H");
+    int current_x_position = 0;
+    int current_y_position = 0;
+    while(current_y_position <= MAX_HEIGHT) {
+        putchar('.');
 
-        i -= (i >= 10 ? i : 0);
-        printf("yupyup%d", i);
-        sleep(1);
-        // msleep(1000);
-    } */
+        if(current_x_position >= MAX_WIDTH) {
+            putchar('\n');
 
-    msleep(1000);
-    printf("send help\n");
-
-    return 0;
-}
-
-int msleep(long period)
-{
-    struct timespec ts;
-    int res;
-
-    if (period < 0) {
-        errno = EINVAL;
-        return -1;
+            current_x_position = 0;
+            current_y_position++;
+        }
+        current_x_position++;
     }
 
-    ts.tv_sec = period / 1000;
-    ts.tv_nsec = (period % 1000) * 1000000;
-
-    do {
-        res = nanosleep(&ts, &ts);
-    } while (res && errno == EINTR);
-
-    return res;
+    return 0;
 }
