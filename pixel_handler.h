29470951808b70f0571ef1fb2a4e0d3392f012b7
Lucas Merritt <lucas.j.merritt@gmail.com>
2023-01-20 17:16:24 -0700

    unfinished documentation and framework

---
+++ pixel_handler.h
@@ -0,0 +1,54 @@
+// rgb color holder used for pixels
+struct rgb_color {
+    int red : 8;    // intensity of red
+    int green : 8;  // intensity of green
+    int blue : 8;   // intensity of blue
+}
+
+// pixel struct passed into pixel related functions and stored
+// uses a screen coordinate system
+// example of a ~(700 x 500) screen using the screen coordinate system:
+//     0 <----------> 700
+//     __________________
+// 0   |................|
+// |   |................|
+// |   |................|
+// |   |................|
+// |   |................|
+// 500 ------------------
+struct pixel {
+    unsigned int x;          // x position of the pixel
+    unsigned int y;          // y position of the pixel
+    struct rgb_color color;  // rgb color of the pixel
+}
+
+// make a rgb_collor struct using the given arguments
+//
+// @param red intensity of red
+// @param green intensity of green
+// @param blue intensity of blue
+// @return made rgb_color struct
+struct rgb_color make_color_struct(int red, int green, int blue);
+
+// make a pixel struct using the given arguments
+// uses screen coordinate system
+//
+// @param x x position of the pixel
+// @param y y position of the pixel
+// @param color struct rgb_color of the pixel color
+// @return made pixel struct
+struct pixel internal_make_pixel_struct(int x, int y, struct rgb_color color);
+
+// create and store a pixel with the supplied arguments
+// uses logical coordinate system
+// example of a ~(700 x 500) screen using the logical coordinate system:
+//              |
+//              |
+//              |
+//              *
+// 
+//
+// @param x_pos x position of the created pixel
+// @param y_pos y position of the created pixel
+// @return success
+int create_pixel(int x_pos, int y_pos, struct rgb_color color);
---
+++ pixel_storage.h
@@ -0,0 +1,24 @@
+#include "pixel_handler.h"
+
+// entry struct in the drawn pixels linked list
+struct pixel_entry {
+    extern struct pixel pixel_node;  // the pixel data to be stored
+    struct pixel_entry *next_entry;  // the next entry in the linked list
+}
+
+// insert an pixel into the drawn pixels linked list(make an entry)
+//
+// @param pixel_node pixel data to insert into the linked list
+// @return pointer to entry
+struct pixel_entry *insert_entry(extern struct pixel pixel_node);
+
+// remove and free an entry from the drawn pixels linked list
+//
+// @param removal_entry entry to remove and free
+void remove_entry(struct pixel_entry removal_entry);
+
+// draw the screen with the given points
+//
+// @param pointer to the start of the linked list containing drawn pixels
+// @return 0 = succeeded >0 = failed, usually due to a pixel being out of bounds
+//int draw_screen(TODO);
