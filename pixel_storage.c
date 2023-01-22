117c7c44902843a67d7a0ce606c0b8f23cae031b
Lucas Merritt <lucas.j.merritt@gmail.com>
2023-01-22 15:10:30 -0700

    implement and refactor pixel_storage functions

---
+++ pixel_storage.c
@@ -0,0 +1,72 @@
+#include "stdlib.h"
+#include "pixel_storage.h"
+#include "pixel_handler.h"
+
+struct pixel_entry *head_entry = NULL;
+
+struct pixel_entry *insert_entry(struct pixel node) {
+    struct pixel_entry *restore_head_entry = *head_entry;  // copy of head entry
+    struct pixel_entry *entry = (struct pixel_entry *)malloc(sizeof(struct pixel_entry));
+
+    entry->pixel_node =_node;
+    entry->next_entry = NULL;
+
+    if (head_entry == NULL) {
+        // no head entry yet
+        goto insert_func_end;
+    }
+
+    // there is already a entry in the list
+    // decide on an entry's position through the x and y positions due to how the screen draws pixels
+    int found_place  // track if a place for the entry has been found 
+    do {
+        // if the entry's y and x are less than or equal to head_entry's x and y
+        found_place = (entry->pixel_node->y <= head_entry->pixel_node->y && entry->pixel_node->x <= head_entry->pixel_node);
+
+        if (found_place) {
+            head_entry->*next_entry = *entry;
+            entry->*next_entry = *head_entry;
+
+            goto insert_func_end;
+        }
+        *head_entry = head_entry->*next_entry;
+    } while (!found_place);
+    // restore head_entry after incrementing
+    *head_entry = *restore_head_entry;
+
+    insert_func_end:
+        *head_entry = *entry;
+        
+        return entry;
+}
+
+void remove_entry(struct pixel_entry *removal_entry) {
+    struct pixel_entry *restore_head_entry = *head_entry;  // copy of head entry
+    // search for entry in linked list
+    int found_entry;  // track if the entry has been found 
+    do {
+        // if head_entry's next entry is the removal entry, or if head_entry is the removal entry
+        found_entry = (head_entry->*next_entry == *removal_entry) || (*head_entry == *removal_entry);
+
+        if (found_entry) {
+            head_entry->*next_entry = removal_entry->*next_entry;
+            // update head_entry if it's being removed
+            if (*head_entry == *removal_entry) {
+                *head_entry = head_entry->*next_entry;
+            }
+
+            goto remove_func_end;
+        }
+
+        *head_entry = head_entry->*next_entry;
+    } while (!found_entry);
+    // restore head_entry after incrementing
+    *head_entry = *restore_head_entry;
+    
+    remove_func_end:
+        free(removal_entry);
+}
+
+int draw_screen(struct pixel_entry *head_draw_entry) {
+    // TODO: move and improve main function drawing code
+}
--- pixel_storage.h
+++ pixel_storage.h
@@ -8,17 +8,17 @@ struct pixel_entry {
 
 // insert an pixel into the drawn pixels linked list(make an entry)
 //
-// @param pixel_node pixel data to insert into the linked list
+// @param node pixel data to insert into the linked list
 // @return pointer to entry
-struct pixel_entry *insert_entry(struct pixel pixel_node);
+struct pixel_entry *insert_entry(struct pixel node);
 
 // remove and free an entry from the drawn pixels linked list
 //
-// @param removal_entry entry to remove and free
-void remove_entry(struct pixel_entry removal_entry);
+// @param removal_entry pointer to entry to remove and free
+void remove_entry(struct pixel_entry *removal_entry);
 
 // draw the screen with the given points
 //
 // @param pointer to the start of the linked list containing drawn pixels
 // @return success
-int draw_screen(struct pixel_entry *head_entry);
+int draw_screen(struct pixel_entry *head_draw_entry);
